687421ce67c9a9e71db22819300a447fa521919b
cls315 <camilasaya315@gmail.com>
2024-02-28 10:53:14 -0300

    liever eventos

--- Front/src/Components/SearchBar/SearchBar.jsx
+++ Front/src/Components/SearchBar/SearchBar.jsx
@@ -38,7 +38,7 @@ const SearchBar = () => {
     <div>
       <nav className="navbar bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand" href="/">
             <img
               src="/docs/5.3/assets/brand/bootstrap-logo.svg"
               alt="Bootstrap"
